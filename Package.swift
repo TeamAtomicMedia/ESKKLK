09496a7c2e86bb40f9306a11d429910049c81842
Anka <runner@Mac-1743778818650.local>
2025-04-04 15:14:47 +0000

    KMP SPM package release for 0.1.7

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/TeamAtomicMedia/ESKKLK/releases/assets/243559552.zip"
+let remoteKotlinChecksum = "4b26db4a9476ff5d4c24dbd6578e144f84b87feaa772fb5c267bf8ffa8ebc5df"
+let packageName = "shared"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
