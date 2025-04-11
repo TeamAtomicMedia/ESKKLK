213551d36b5fc797db384281eaea4e6bb66d0643
Elliot Murray <elliot@atomicmedia.co.uk>
2025-04-11 17:25:28 +0100

    .

--- shared/src/iosMain/kotlin/uk/co/atomicmedia/eskklk/shared/IosAppComponent.kt
+++ shared/src/iosMain/kotlin/uk/co/atomicmedia/eskklk/shared/IosAppComponent.kt
@@ -1,5 +1,6 @@
 package uk.co.atomicmedia.eskklk.shared
 
+import me.tatarka.inject.annotations.Provides
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
 import software.amazon.lastmile.kotlin.inject.anvil.MergeComponent
 import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
@@ -7,7 +8,7 @@ import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 @MergeComponent(AppScope::class)
 @SingleIn(AppScope::class)
 abstract class IosAppComponent(
-    @get:Provides appVersionProvider: AppVersionProvider,
+    @get:Provides val appVersionProvider: AppVersionProvider,
 ) : AppComponent
 
 @MergeComponent.CreateComponent
