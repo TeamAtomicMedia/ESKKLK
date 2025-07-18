e7008ce73bbed1065eee2c3a1d42d5fbf673cb8e
Elliot Murray <elliot@atomicmedia.co.uk>
2025-07-18 12:28:46 +0100

    Bump lib versions, removed android build

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
-    alias(libs.plugins.android.library)
+//    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kmmbridge)
     alias(libs.plugins.skie)
@@ -16,12 +16,12 @@ plugins {
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
-        publishAllLibraryVariants()
-    }
+//    androidTarget {
+//        compilerOptions {
+//            jvmTarget.set(JvmTarget.JVM_17)
+//        }
+//        publishAllLibraryVariants()
+//    }
     listOf(
         iosX64(),
         iosArm64(),
@@ -52,13 +52,13 @@ kotlin {
 dependencies {
     kspCommonMainMetadata(libs.kotlin.inject.compiler.ksp)
 
-    add("kspAndroid", libs.kotlin.inject.compiler.ksp)
+//    add("kspAndroid", libs.kotlin.inject.compiler.ksp)
     add("kspIosX64", libs.kotlin.inject.compiler.ksp)
     add("kspIosArm64", libs.kotlin.inject.compiler.ksp)
     add("kspIosSimulatorArm64", libs.kotlin.inject.compiler.ksp)
 
 //    kspCommonMainMetadata(libs.anvil.compiler)
-    add("kspAndroid", libs.anvil.compiler)
+//    add("kspAndroid", libs.anvil.compiler)
     add("kspIosX64", libs.anvil.compiler)
     add("kspIosArm64", libs.anvil.compiler)
     add("kspIosSimulatorArm64", libs.anvil.compiler)
@@ -81,29 +81,29 @@ skie {
     }
 }
 
-android {
-    namespace = "uk.co.atomicmedia.eskklk"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-}
+//android {
+//    namespace = "uk.co.atomicmedia.eskklk"
+//    compileSdk = 35
+//
+//    defaultConfig {
+//        minSdk = 24
+//    }
+//
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_17
+//        targetCompatibility = JavaVersion.VERSION_17
+//    }
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
+//}
 
 apollo {
     service("service") {
